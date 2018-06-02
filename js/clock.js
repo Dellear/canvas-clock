73d6e1df6b559a3952e5e7ea62120e4ec4f88f80
wang <358009775@qq.com>
2018-06-03 01:04:07 +0800

    采用原型改写时钟

--- js/clock.js
+++ js/clock.js
@@ -1,24 +1,66 @@
-function init(option) { // eslint-disable-line
+(function (window, document) {
     'use strict';
-    var container = document.getElementById('container'),
-        clockbg = document.createElement('canvas'),
-        clock = document.createElement('canvas');
-    clockbg.id = 'clockbg';
-    clock.id = 'clock';
-    container.style.position = 'relative';
-    clockbg.style.position = clock.style.position = 'absolute';
-    container.style.width = container.style.height = option.width + 'px';
-    clockbg.style.left = clockbg.style.top = clock.style.left = clock.style.top = 0;
-    clockbg.style.width = clockbg.style.height = clock.style.width = clock.style.height = option.width + 'px';
-    container.appendChild(clockbg);
-    container.appendChild(clock);
 
-    var ctxbg = clockbg.getContext('2d'),
-        ctx = clock.getContext('2d'),
-        r = option.width / 2; //r=100
-    clockbg.width = clockbg.height = clock.width = clock.height = 2 * r;
+    function Clock(option) {
+        this.id = option.id;
+        this.option = option;
+        this.r = this.option.width / 2;
+        this.clock = null;
+        this.ctx = null;
+        this.clockbg = null;
+        this.ctxbg = null;
+        this.interval = null;
+        this.init();
+    }
 
-    function drawBackground() {
+    if(!window.Clock) {
+        window.Clock = Clock;
+    }
+
+    /**
+     * 初始化
+     */
+    Clock.prototype.init = function () {
+        var T = this;
+        this.destroy();
+        this.createContainer();
+        this.drawBackground();
+        this.draw();
+        this.interval = setInterval(function () {
+            T.draw();
+        }, 1000);
+    }
+
+    /**
+     * 销毁时钟
+     */
+    Clock.prototype.destroy = function () {
+        if(this.clock) {
+            this.clock.parentNode.removeChild(this.clock);
+            this.clock = null;
+        }
+        if(this.clockbg) {
+            this.clockbg.parentNode.removeChild(this.clockbg);
+            this.clockbg = null;
+        }
+
+        window.clearInterval(this.interval);
+    }
+
+    /**
+     * 创建容器
+     */
+    Clock.prototype.createContainer = function () {
+        this.createDoms();
+        this.initSize();
+    }
+
+    /**
+     * 绘制背景
+     */
+    Clock.prototype.drawBackground = function () {
+        var r = this.r,
+            ctxbg = this.ctxbg;
         ctxbg.translate(r, r);
         ctxbg.lineWidth = r / 15;
         ctxbg.beginPath();
@@ -26,7 +68,7 @@ function init(option) { // eslint-disable-line
         ctxbg.stroke();
 
         var hourNumbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
-        hourNumbers.forEach(function(number, i) {
+        hourNumbers.forEach(function (number, i) {
             ctxbg.font = r / 7 + 'px Arial';
             ctxbg.textAlign = 'center';
             ctxbg.textBaseline = 'middle';
@@ -52,8 +94,62 @@ function init(option) { // eslint-disable-line
         }
     }
 
-    function drawFont(date) {
-        var monthArr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+    /**
+     * 绘制完整的时钟（包括表针、日期、星期等）
+     */
+    Clock.prototype.draw = function () {
+        var now = new Date();
+
+        this.preDraw();
+        this.drawFont(now);
+        this.drawHour(now);
+        this.drawMinute(now);
+        this.drawSecond(now);
+        this.drawDot();
+    }
+
+    /**
+     * 创建相关的DOM元素
+     */
+    Clock.prototype.createDoms = function () {
+        var container = document.getElementById(this.id),
+            clockbg = document.createElement('canvas'),
+            clock = document.createElement('canvas');
+        clockbg.id = 'clockbg';
+        clock.id = 'clock';
+        container.style.position = 'relative';
+        clockbg.style.position = clock.style.position = 'absolute';
+        container.style.width = container.style.height = this.option.width + 'px';
+        clockbg.style.left = clockbg.style.top = clock.style.left = clock.style.top = 0;
+        clockbg.style.width = clockbg.style.height = clock.style.width = clock.style.height = this.option.width + 'px';
+        container.appendChild(clockbg);
+        container.appendChild(clock);
+
+        this.clock = clock;
+        this.clockbg = clockbg;
+    }
+
+    /**
+     * 初始化大小
+     */
+    Clock.prototype.initSize = function () {
+        var r = this.r;
+
+        this.ctxbg = this.clockbg.getContext('2d'),
+            this.ctx = this.clock.getContext('2d'),
+            this.clockbg.width = 2 * r;
+        this.clockbg.height = 2 * r;
+        this.clock.width = 2 * r;
+        this.clock.height = 2 * r;
+    }
+
+    /**
+     * 绘制日期和星期几
+     */
+    Clock.prototype.drawFont = function (date) {
+        var ctx = this.ctx,
+            r = this.r,
+            monthArr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
             weekArr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
             year = date.getFullYear(),
             month = monthArr[date.getMonth()],
@@ -69,13 +165,19 @@ function init(option) { // eslint-disable-line
         ctx.restore();
     }
 
-    function drawHour(date) {
-        var hour = date.getHours(),
-            minute = date.getMinutes();
+    /**
+     * 绘制时针
+     */
+    Clock.prototype.drawHour = function (date) {
+        var ctx = this.ctx,
+            r = this.r,
+            hour = date.getHours(),
+            minute = date.getMinutes(),
+            rad = 2 * Math.PI / 12 * hour,
+            mrad = 2 * Math.PI / 12 / 60 * minute;
         ctx.save();
         ctx.beginPath();
-        var rad = 2 * Math.PI / 12 * hour,
-            mrad = 2 * Math.PI / 12 / 60 * minute;
+
         ctx.rotate(rad + mrad);
         ctx.lineWidth = r / 20;
         ctx.lineCap = 'round';
@@ -85,13 +187,19 @@ function init(option) { // eslint-disable-line
         ctx.restore();
     }
 
-    function drawMinute(date) {
-        var minute = date.getMinutes(),
-            second = date.getSeconds();
+    /**
+     * 绘制分针
+     */
+    Clock.prototype.drawMinute = function (date) {
+        var ctx = this.ctx,
+            r = this.r,
+            minute = date.getMinutes(),
+            second = date.getSeconds(),
+            rad = 2 * Math.PI / 60 * minute,
+            srad = 2 * Math.PI / 60 / 60 * second;
         ctx.save();
         ctx.beginPath();
-        var rad = 2 * Math.PI / 60 * minute,
-            srad = 2 * Math.PI / 60 / 60 * second;
+
         ctx.rotate(rad + srad);
         ctx.lineWidth = r / 33;
         ctx.lineCap = 'round';
@@ -101,12 +209,19 @@ function init(option) { // eslint-disable-line
         ctx.restore();
     }
 
-    function drawSecond(date) {
-        var second = date.getSeconds();
+    /**
+     * 绘制秒针
+     */
+    Clock.prototype.drawSecond = function (date) {
+        var ctx = this.ctx,
+            r = this.r,
+            second = date.getSeconds(),
+            rad = 2 * Math.PI / 60 * second;
+
         ctx.save();
         ctx.beginPath();
         ctx.fillStyle = '#f00';
-        var rad = 2 * Math.PI / 60 * second;
+
         ctx.rotate(rad);
         ctx.moveTo(-r / 50, r / 5);
         ctx.lineTo(r / 50, r / 5);
@@ -116,27 +231,21 @@ function init(option) { // eslint-disable-line
         ctx.restore();
     }
 
-    function drawDot() {
-        ctx.beginPath();
-        ctx.fillStyle = '#fff';
-        ctx.arc(0, 0, r / 33, 0, Math.PI * 2, false);
-        ctx.fill();
+    /**
+     * 绘制表针的交汇固定点
+     */
+    Clock.prototype.drawDot = function drawDot() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = '#fff';
+        this.ctx.arc(0, 0, this.r / 33, 0, Math.PI * 2, false);
+        this.ctx.fill();
+        this.ctx.restore();
     }
 
-    function draw() {
-        var now = new Date();
-        ctx.clearRect(0, 0, 2 * r, 2 * r);
-        ctx.save();
-        ctx.translate(r, r);
-        drawFont(now);
-        drawHour(now);
-        drawMinute(now);
-        drawSecond(now);
-        drawDot();
-        ctx.restore();
+    Clock.prototype.preDraw = function () {
+        this.ctx.clearRect(0, 0, this.option.width, this.option.width);
+        this.ctx.save();
+        this.ctx.translate(this.r, this.r);
     }
 
-    drawBackground();
-    draw();
-    setInterval(draw, 1000);
-}
+})(window, document);
